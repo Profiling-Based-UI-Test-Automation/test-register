11758a845ddac86041bdcadf8750aeabddc38f29
heeseon <domich.hwang@gmail.com>
2018-10-03 15:15:37 +0900

    add apis to register, delete, update, read tc

---
+++ src/main/java/testregister/model/Test.java
@@ -0,0 +1,104 @@
+package testregister.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.Data;
+
+@Data
+public class Test {
+	@Id
+	private String _id;
+	
+	private String testId;
+	
+	private String testName;
+	
+	private String testDescription;
+	
+	private String appId;
+	
+	private ObjectId apkId; 
+	
+	private ObjectId tcId;
+	
+	private LocalDateTime registerDate;
+	
+	public Test(){
+		testId = UUID.randomUUID().toString();	
+		registerDate = LocalDateTime.now();
+	}
+	
+	public String getTestId() {
+		return testId;
+	}
+
+	private void setTestId(String testId) {
+		this.testId = testId;
+	}
+	
+	public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String _appId) {
+		this.appId = _appId;
+	}
+	
+	public String getTestName() {
+		return testName;
+	}
+
+	public void setTestName(String _testName) {
+		this.testName = _testName;
+	}
+	
+	public String getTestDescription() {
+		return testDescription;
+	}
+
+	public void setTestDescription(String _testDescription) {
+		this.testDescription = _testDescription;
+	}
+	
+	public LocalDateTime getRegDate() {
+	    return registerDate;
+	}
+	public void setRegDate(LocalDateTime regDate) {
+	    this.registerDate = regDate;
+	}
+
+	public ObjectId getApkId() {
+		return apkId;
+	}
+
+	public void setApkId(ObjectId _apkId) {
+		this.apkId = _apkId;
+	}
+	
+	public ObjectId getTcId() {
+		return tcId;
+	}
+
+	public void setTcId(ObjectId _tcId) {
+		this.tcId = _tcId;
+	}	
+	
+	
+	@Override
+	public String toString() {
+		return "Test [ testId = " + testId + ", testName = " + testName + ", testDescription = " + testDescription + ", appId = " + appId   + ", apkId = " + apkId + ", tcId = " + tcId + ", registerDate = " + registerDate +  "]";	
+	}
+
+}
---
+++ src/main/java/testregister/userInput/TestInput.java
@@ -0,0 +1,85 @@
+package testregister.userInput;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.Data;
+
+
+public class TestInput {
+	@Id
+	private String _id;
+		
+	private String testName;
+	
+	private String testDescription;
+	
+	private String appId;
+	
+	private ObjectId apkId; 
+	
+	private ObjectId tcId;
+	
+	
+	public TestInput(){
+
+	}
+	
+	public String getAppId() {
+		return appId;
+	}
+
+	private void setAppId(String _appId) {
+		this.appId = _appId;
+	}
+	
+	public String getTestName() {
+		return testName;
+	}
+
+	public void setTestName(String _testName) {
+		this.testName = _testName;
+	}
+	
+	public String getTestDescription() {
+		return testDescription;
+	}
+
+	public void setTestDescription(String _testDescription) {
+		this.testDescription = _testDescription;
+	}
+
+	public ObjectId getApkId() {
+		return apkId;
+	}
+
+	public void setApkId(ObjectId _apkId) {
+		this.apkId = _apkId;
+	}
+	
+	public ObjectId getTcId() {
+		return tcId;
+	}
+
+	public void setTcId(ObjectId _tcId) {
+		this.tcId = _tcId;
+	}	
+	
+	
+	@Override
+	public String toString() {
+		return "Test [ testName = " + testName + ", testDescription = " + testDescription + ", appId = " + appId   + ", apkId = " + apkId + ", tcId = " + tcId + "]";	
+	}
+
+}
